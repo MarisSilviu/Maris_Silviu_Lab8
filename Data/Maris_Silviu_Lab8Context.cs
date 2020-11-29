eac3373b3011a03425583edc37321e526da8770c
Bogdan Maris <bogdanmaris11@yahoo.com>
2020-11-29 15:44:28 +0200

    Add project files.

---
+++ Data/Maris_Silviu_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Maris_Silviu_Lab8.Models;
+
+namespace Maris_Silviu_Lab8.Data
+{
+    public class Maris_Silviu_Lab8Context : DbContext
+    {
+        public Maris_Silviu_Lab8Context (DbContextOptions<Maris_Silviu_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Maris_Silviu_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Maris_Silviu_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Maris_Silviu_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Maris_Silviu_Lab8.Data;
+using Maris_Silviu_Lab8.Models;
+
+namespace Maris_Silviu_Lab8.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Maris_Silviu_Lab8.Data.Maris_Silviu_Lab8Context _context;
+
+        public DetailsModel(Maris_Silviu_Lab8.Data.Maris_Silviu_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
